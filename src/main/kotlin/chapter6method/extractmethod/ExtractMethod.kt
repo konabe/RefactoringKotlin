c682a93f013a02998cd398719c9c033cddced7b9
Ryota Konabe <rt.konabe@gmail.com>
2021-08-29 07:25:08 +0900

    implement ExtractMethod Before

---
+++ src/main/kotlin/chapter6method/extractmethod/ExtractMethod.kt
@@ -0,0 +1,49 @@
+package chapter6method.extractmethod
+
+data class Order(
+    val amount: Double
+)
+
+interface Printable {
+    fun println(text: String)
+}
+
+class Printer: Printable {
+    override fun println(text: String) {
+        kotlin.io.println(text)
+    }
+}
+
+class Accounting(
+    private val name: String,
+    private val orders: List<Order>,
+    private val printer: Printable
+) {
+    fun printOwing() {
+
+        printer.println("***************") // *x15
+        printer.println("* Custom Owes *")
+        printer.println("***************")
+
+        val outstanding: Double = orders
+            .map { it.amount }
+            .reduce { t, v ->
+            t + v
+        }
+
+        printer.println("name: $name")
+        printer.println("amount: $outstanding")
+    }
+
+}
+
+fun main() {
+    val orders = listOf(
+        Order(10_000.0),
+        Order(5_000.0),
+        Order(20_000.0),
+        Order(1_000.0)
+    )
+    val accounting = Accounting("John", orders, Printer())
+    accounting.printOwing()
+}
---
+++ src/test/kotlin/ExtractMethodTest.kt
@@ -0,0 +1,45 @@
+import chapter6method.extractmethod.Accounting
+import chapter6method.extractmethod.Order
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+internal class ExtractMethodTest {
+    private lateinit var printer: MockPrinter
+    private val normalOrders = listOf(
+        Order(10_000.0),
+        Order(5_000.0),
+        Order(20_000.0),
+        Order(1_000.0)
+    )
+    private val normalName = "John"
+
+    @BeforeEach
+    fun init() {
+        printer = MockPrinter()
+    }
+
+    @Test
+    fun testPrintOwing() {
+        val accounting = Accounting(
+            normalName, normalOrders, printer
+        )
+        accounting.printOwing()
+        assertEquals("***************", printer.printlnVarArgs[0])
+        assertEquals("* Custom Owes *", printer.printlnVarArgs[1])
+        assertEquals("***************", printer.printlnVarArgs[2])
+        assertEquals("name: John", printer.printlnVarArgs[3])
+        assertEquals("amount: 36000.0", printer.printlnVarArgs[4])
+    }
+}
+
+private class MockPrinter: chapter6method.extractmethod.Printable {
+    var printlnCallCount = 0
+    var printlnVarArgs: MutableList<String> = mutableListOf()
+    override fun println(text: String) {
+        printlnCallCount += 1
+        printlnVarArgs.add(text)
+    }
+
+}
